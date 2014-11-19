84b725007dc66e3e8e0298693cbfba74211490da
khrystynaklochko <khrystynaklochko@gmail.com>
2014-11-19 20:56:15 +0200

    add ajax

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -15,7 +15,7 @@ class Cart < ActiveRecord::Base
   def total_price
      line_items.to_a.sum { |item| item.total_price }
   end
-  
+
 private
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,6 +2,8 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
   def total_price
-    product.price * quantity 
+    if quantity.present?
+      product.price * quantity 
+    end
   end
 end
