516e2c577d985a4d23885a03ba7037fc80fc4de0
khrystynaklochko <khrystynaklochko@gmail.com>
2014-11-19 12:45:54 +0200

    add errors

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,21 @@
 class Cart < ActiveRecord::Base
   has_many :line_items
   has_many :line_items, dependent: :destroy
+   
+  def add_product(product_id)
+     current_item = line_items.find_by(product_id: product_id)
+     if current_item
+         current_item.quantity += 1
+     else
+         current_item = line_items.build(product_id: product_id)
+     end
+     current_item
+  end
 
+  def total_price
+     line_items.to_a.sum { |item| item.total_price }
+  end
+  
 private
   def ensure_not_referenced_by_any_line_item
     if line_items.empty?
